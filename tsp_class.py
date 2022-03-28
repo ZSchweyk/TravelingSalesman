b7c9092c6c73361fe169d14ce27cece22111e57c
ZSchweyk <zeynschweyk@dpengineering.org>
2022-03-28 11:06:27 -0700

    made better brute force method

--- tsp_class.py
+++ tsp_class.py
@@ -27,7 +27,30 @@ class TravelingSalesman:
 
         shortest_path = min(paths, key=paths.get)
         shortest_path_cost = paths[shortest_path]
-        return shortest_path, shortest_path_cost
+        return [start] + list(shortest_path) + [start], shortest_path_cost
+
+    def better_brute_force(self, **kwargs):
+        method = kwargs["method"]
+        start: Node = kwargs["start"]
+        end: Node = kwargs["end"]
+        all_permutations = permutations([node for node in self.nodes if node != start and node != end])
+
+        paths = {}
+        for permutation in all_permutations:
+            total_cost = 0
+            for i in range(len(permutation)):
+                if i == len(permutation) - 1:
+                    break
+                current_node: Node = permutation[i]
+                next_node: Node = permutation[i+1]
+                total_cost += current_node.branches[next_node][method]
+
+            total_cost += start.branches[permutation[0]][method] + permutation[-1].branches[end][method]
+            paths[permutation] = total_cost
+
+        shortest_path = min(paths, key=paths.get)
+        shortest_path_cost = paths[shortest_path]
+        return [start] + list(shortest_path) + [end], shortest_path_cost
 
 
 
