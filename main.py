44df9466aa219d48427be54ae556e04684f2e12d
ZSchweyk <zeynschweyk@dpengineering.org>
2022-03-29 08:28:44 -0700

    created an api key

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import time
 from itertools import combinations
 import requests, json
@@ -18,8 +19,8 @@ colleges = [
     "Cornell University",
     "University of Pennsylvania",
     "Tufts University",
-    "Duke University",
-    "Brown University",
+    # "Duke University",
+    # "Brown University",
 ]
 
 Node.bi_directional = True
@@ -28,10 +29,21 @@ college_nodes = [Node(college) for college in colleges]
 
 all_combinations = list(combinations(college_nodes, 2))
 
+api_key = "AIzaSyD3YANW_cRnM-TinIgfUjtP8TWNgFIj1K8"
+url ='https://maps.googleapis.com/maps/api/distancematrix/json?'
+
 for college in college_nodes:
     for combination in all_combinations:
         if college == combination[0]:
-            college.cost_to(combination[1], distance=10, time=10)
+            r = requests.get(url + 'origins = ' + college.name +
+                             '&destinations = ' + combination[1].name +
+                             '&key = ' + api_key)
+            x = r.json()
+            print(x)
+            x = x['rows'][0]['elements'][0]
+
+
+            college.cost_to(combination[1], distance=r["distance"]["value"], time=r["duration"]["value"] / (60 * 60))
 
 graph = TravelingSalesman(college_nodes)
 print("Created graph")
