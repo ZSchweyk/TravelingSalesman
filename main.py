4dc840d0519f27979db0136a9aacffbd60340cf6
ZSchweyk <zeynschweyk@dpengineering.org>
2022-03-30 22:37:13 -0700

    works on one iteration, not the others...

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from tsp_class import TravelingSalesman
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+
 # https://www.geeksforgeeks.org/python-calculate-distance-duration-two-places-using-google-distance-matrix-api/
 # https://distancecalculator.globefeed.com/US_Distance_Calculator.asp
 
@@ -28,19 +29,12 @@ colleges = [
     # "Brown University",
 ]
 
-
-
-
 browser = webdriver.Chrome()
 browser.get("https://distancecalculator.globefeed.com/US_Distance_Calculator.asp")
 
 loc1_field = browser.find_element(by=By.ID, value="placename1")
 loc2_field = browser.find_element(by=By.ID, value="placename2")
 submit_button = browser.find_elements(by=By.TAG_NAME, value="button")[1]
-
-print(f"{loc1_field=}")
-print(f"{loc2_field=}")
-print(f"{submit_button=}")
 
 Node.bi_directional = True
 college_nodes = [Node(college) for college in colleges]
@@ -56,14 +50,18 @@ for college in college_nodes:
             submit_button.click()
 
             # New Page with Results
+            driving_distance = float(
+                browser.find_element(by=By.ID, value="drvDistance").text.split(sep=" ")[0])  # miles
+            print(f"{driving_distance=}")
+            driving_duration = browser.find_element(by=By.ID, value="drvDuration").text.split(sep=" ")
+            print(f"{driving_duration=}")
+            driving_duration = int(driving_duration[0]) + float(driving_duration[2]) / 60  # hours
+            college.cost_to(combination[1], distance=driving_distance, time=driving_duration)
 
-            while True:
-                pass
-            college.cost_to(combination[1], distance=10, time=10)
+            browser.back()
 
 graph = TravelingSalesman(college_nodes)
 print("Created graph")
 print("Starting brute force algorithm...")
 shortest_path = graph.brute_force(start="University of Rochester", end="University of Rochester", method="distance")
 print("Shortest Path:", shortest_path)
-
