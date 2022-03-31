d903fd3a161a67a4f582d3b294a90baf65d34594
ZSchweyk <zeynschweyk@dpengineering.org>
2022-03-30 22:23:22 -0700

    finally; some progress! I was grabbing the facebook like button, not the form submit button!!!

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import random
 import time
 from itertools import combinations
+
+import selenium.common.exceptions
+
 from node_class import Node
 from tsp_class import TravelingSalesman
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 # https://www.geeksforgeeks.org/python-calculate-distance-duration-two-places-using-google-distance-matrix-api/
 # https://distancecalculator.globefeed.com/US_Distance_Calculator.asp
 
@@ -25,7 +31,16 @@ colleges = [
 
 
 
+browser = webdriver.Chrome()
+browser.get("https://distancecalculator.globefeed.com/US_Distance_Calculator.asp")
 
+loc1_field = browser.find_element(by=By.ID, value="placename1")
+loc2_field = browser.find_element(by=By.ID, value="placename2")
+submit_button = browser.find_elements(by=By.TAG_NAME, value="button")[1]
+
+print(f"{loc1_field=}")
+print(f"{loc2_field=}")
+print(f"{submit_button=}")
 
 Node.bi_directional = True
 college_nodes = [Node(college) for college in colleges]
@@ -34,7 +49,16 @@ all_combinations = list(combinations(college_nodes, 2))
 for college in college_nodes:
     for combination in all_combinations:
         if college == combination[0]:
+            loc1_field.send_keys(college.name)
+            # webdriver.ActionChains(browser).send_keys(Keys.ESCAPE).perform()
+            loc2_field.send_keys(combination[1].name)
+            # webdriver.ActionChains(browser).send_keys(Keys.ESCAPE).perform()
+            submit_button.click()
 
+            # New Page with Results
+
+            while True:
+                pass
             college.cost_to(combination[1], distance=10, time=10)
 
 graph = TravelingSalesman(college_nodes)
